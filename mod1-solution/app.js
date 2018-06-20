c93bc3b4d5cfce3d2d3e0831f1d838add07d67dd
Abhishek Sinha <abhishek@assetplus.io>
2018-06-20 14:19:27 +0530

    added regular expression

--- mod1-solution/app.js
+++ mod1-solution/app.js
@@ -13,23 +13,30 @@
             "color": "red"
         }
         $scope.check = function (stringToSplit) {
-            var lunchMenu = stringToSplit.split(',');
-            
-            //cleaning the empty elements
-            Array.prototype.clean = function(deleteValue) {
-                for (var i = 0; i < this.length; i++) {
-                  if (this[i] == deleteValue) {         
-                    this.splice(i, 1);
-                    i--;
-                  }
-                }
-                return this;
-            };              
-            lunchMenu.clean('');
-            //console.log(lunchMenu);
-            $scope.message = (lunchMenu.length < 4) ? "Enjoy!" : "Too much!";
-            $scope.color = (lunchMenu.length < 4) ? "green" : "red";
-        }
 
+            //console.log(lunchMenu);
+            if (stringToSplit && stringToSplit != "") {
+                var str = stringToSplit.replace(/\s\s+/g, '');
+                var lunchMenu = str.split(',');
+                //cleaning the empty elements
+                Array.prototype.clean = function (deleteValue) {
+                    for (var i = 0; i < this.length; i++) {
+                        if (this[i] == deleteValue) {
+                            this.splice(i, 1);
+                            i--;
+                        }
+                    }
+                    return this;
+                };
+                lunchMenu.clean('');
+                lunchMenu.clean(' ');
+                $scope.message = (lunchMenu.length < 4) ? "Enjoy!" : "Too much!";
+                $scope.color = (lunchMenu.length < 4) ? "green" : "red";
+            }
+            else {
+                $scope.message = "Please enter data first";
+                $scope.color = "red";
+            }
+        }
     };
 })();
